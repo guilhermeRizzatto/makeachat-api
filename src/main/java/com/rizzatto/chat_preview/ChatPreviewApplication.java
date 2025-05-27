830bb9755d1903d597995c1d4ecdcf7be362247b
¨Guilherme <guimarquesrz@gmail.com>
2025-05-26 21:11:41 -0300

    Initial commit

---
+++ src/main/java/com/rizzatto/chat_preview/ChatPreviewApplication.java
@@ -0,0 +1,13 @@
+package com.rizzatto.chat_preview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatPreviewApplication.class, args);
+	}
+
+}
